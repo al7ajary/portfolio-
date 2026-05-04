64d4a071726aa28f4e70fbb80cb06c1c548c0ced
al7ajary <al7ajary@gmail.com>
2026-05-04 12:37:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -102,3 +102,13 @@
         });
     }
 })();
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+if (scrollTopBtn) {
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
